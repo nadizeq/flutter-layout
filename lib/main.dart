efa2ae3dd4738e027ceeec472b81aa44d922ec6a
Nadiah Zairee <nadizeq@yahoo.com>
2021-10-28 23:17:42 +0800

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart' as rootBundle;
 import 'package:dayfourflutter/UserDataModel.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
 void main(){
   runApp(MyApp());
 }
@@ -29,82 +31,145 @@ class _MyHomePageState extends State <MyHomePage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: ReadJsonData(),
-       //data that has been read{
-        builder: (context,data) {
-          //this if condition is executed when there are erros when reading
-          //json file
-          if(data.hasError){
-            return Center(
-              child: Text("${data.error}"),
-            );
-          }else if(data.hasData){
-            var items = data.data as List<UserDataModel>;
-            return ListView.builder(
-                itemCount: items == null? 0: items.length,
-                itemBuilder: (context,index){
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(horizontal: 10,
-                    vertical: 6),
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            child: Image(
-                              image: NetworkImage(
-                                items[index].avatar == null
-                                    ? 'https://static.thenounproject.com/png/3134331-200.png'
-                                    : items[index].avatar.toString(),
-                              ),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          Expanded(child: Container(
-                            padding: EdgeInsets.only(bottom: 8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                    padding: EdgeInsets.only(left: 8,right:8),
-                                child: Text(
-                                  items[index].firstName.toString()
-                                      +" "+ items[index].lastName.toString(),
-                                  style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold
-                                ),
-                                ),
-                                ),
-
-                                Padding(padding: EdgeInsets.only(left:8,right:8),
-                                child: Text(items[index].status == null? 'Status' : items[index].status.toString()),)
-                              ],
-                            ),
-                          ),
-                          ),
-                          Expanded(child: Container(
-
-                          ))
-                        ],
-                      ),
-                    ),
-                  );
-                });
-          }else{
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+      backgroundColor: Colors.deepPurpleAccent,
+      appBar: AppBar(
+        title: Text(
+          'Users List'
+        )
       ),
+      body:
+      Center(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.red,
+                  Colors.blue,
+                ],
+              )
+          ),
+          child: FutureBuilder(
+            future: ReadJsonData(),
+            //data that has been read{
+            builder: (context,data) {
+              //this if condition is executed when there are erros when reading
+              //json file
+              if(data.hasError){
+                return Center(
+                  child: Text("${data.error}"),
+                );
+              }else if(data.hasData){
+                var items = data.data as List<UserDataModel>;
+                return ListView.builder(
+                    itemCount: items == null? 0: items.length,
+                    itemBuilder: (context,index){
+                      return Card(
+                        elevation: 5,
+                        margin: EdgeInsets.symmetric(horizontal: 10,
+                            vertical: 6),
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                width: 50,
+                                height: 50,
+                                child: Image(
+                                  image: NetworkImage(
+                                    items[index].avatar == null
+                                        ? 'https://static.thenounproject.com/png/3134331-200.png'
+                                        : items[index].avatar.toString(),
+                                  ),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Expanded(child: Container(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 20,right:8),
+                                      child: Text(
+                                        items[index].firstName.toString()
+                                            +" "+ items[index].lastName.toString(),
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(padding: EdgeInsets.only(left: 20,right:8),
+                                      child: Text(
+                                        items[index].username.toString(),
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),),
+
+                                    Padding(padding: EdgeInsets.only(left:20,right:8),
+                                      child: Text(
+                                        items[index].status == null? 'Status' :
+                                        items[index].status.toString(),
+                                        style: TextStyle(
+                                          fontSize: 10,
+                                          color: Colors.blueGrey,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              ),
+                              Expanded(child: Container(
+                                padding: EdgeInsets.only(bottom: 8),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 8,right:8),
+                                      child: Text(items[index].lastSeenTime.toString(),
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold
+                                        ),
+                                      ),
+                                    ),
+
+                                    Padding(padding: EdgeInsets.only(left:8,right:8),
+                                        child: Text(items[index].messages == null? ' '
+                                            : items[index].messages.toString(),
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold),))
+                                  ],
+                                ),
+                              ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              }else{
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+        ),
+
+      )
+
+
+
     );
   }
 
@@ -120,3 +185,6 @@ class _MyHomePageState extends State <MyHomePage>{
     return list.map((e) => UserDataModel.fromJson(e)).toList();
   }
 }
+
+
+
